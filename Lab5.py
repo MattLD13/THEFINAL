b4caa21758936b91a56d01640e9ea4ec66e5afaf
MattLD13 <mdicks2025@pingry.org>
2023-05-22 13:35:46 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -9,6 +9,7 @@ import random
 # Main function
 def main():
 
+	# define variables
 	questionAmt = 0
 	score = 0
 	pickedQuestions = []
@@ -16,84 +17,136 @@ def main():
 	fileExists = True
 	isChoice = False
 
+	# print the welcome message
 	welcome_message()
+
+	# get the file name
 	fileName = get_initial_input()
+
+	# read the file and create a dictionary
 	questionsDict = read_file(fileName)
+
+	# if the dictionary
 	if(questionsDict == {}):
+		
+		# if the file doesn't exist, print an error message
 		print("Sorry, the file \"" + fileName + "\" does not exist.")
+
+		# set the fileExists variable to false
 		fileExists = False
+
+		# set the question value to 10 to not start the quiz
 		questionAmt = 10
 
+	# while loop to only play 10 questions
 	while(questionAmt < 10):
-		print("Question " + str(questionAmt + 1) + ":")
+		goodInput = False
 
+		# pick a random question using the pick_random
 		chosenQuestion = pick_random(questionsDict, pickedQuestions, isChoice)
 
+		# add the chosen question to the pickedQuestions list
 		pickedQuestions.append(chosenQuestion)
 
-		print("after" + str(pickedQuestions))
-
+		# pick 3 random choices and 1 correct choice using pick_choices
 		choices = pick_choices(chosenQuestion, questionsDict)
 
+		# create the question using create_question
 		question = create_question(chosenQuestion, choices)
-		print(question)
 
-		chosenAnswer = get_question_input()
-		if(chosenAnswer == 0):
-			print("Please enter a valid choice (A, B, C, or D).")
-			print()
+		while(goodInput == False):
 			print(question)
-			chosenAnswer = get_question_input()
 
+			# get the user's answer using get_question_input
+			chosenAnswer, goodInput = get_question_input()
+
+			if(goodInput == False):
+				print("Please enter a valid answer (A, B, C, or D).")
+				print()
+
+		# check if the answer is correct using check_answer
 		isCorrect = check_answer(chosenAnswer, choices, questionsDict, chosenQuestion)
 		
+		# if the answer is correct, print "Correct!" and add 1 to the score
 		if(isCorrect):
 			print("Correct!")
 			score += 1
+
+		# if the answer is incorrect, prints the correct answer
 		else:
 			print("No, the correct answer is " + questionsDict[chosenQuestion] + ".")
 
 		print()
 		questionAmt += 1
+
+	# print the score
 	if(fileExists): 
 		print("Score: " + str(score) + " / " + str(questionAmt))
 
+# welcome_message function that prints the welcome message
 def welcome_message():
+
+	# print the welcome message
 	print("Welcome to the Quiz Game!")
 	print("This game can be used to quiz yourself on different types of facts.")
 	print("You will be shown up to 10 question.   For every correct answer, you \nwill get 1 point. Good luck!")
 	print()
 
+# gets the file name from the user
 def get_initial_input():
+
+	# get the file name from the user
 	fileName = input("Enter the name of the fact file: ")
 	print()
+
+	# return the file name
 	return fileName
 
-def get_question_input():
-	isLooping = True
-	while isLooping:
-		usrInput = input("Enter Choice: ")
-		try:
-			usrInput = str(usrInput)
-		except:
-			print("Please enter a valid choice (A, B, C, or D).")
-		usrInput = usrInput.upper()
-		if(usrInput == "A" or usrInput == "B" or usrInput == "C" or usrInput == "D"):
-			isLooping = False
-			chosenAnswer = ord(usrInput)
-			return chosenAnswer
-		else:
-			return 0
-
+# check if the file exists and read it
 def read_file(fileName):
+
+	# if the file exists, read it and create a dictionary
 	if file_exists(fileName):
+
+		# open the file and read it
 		fo = open(fileName, "r")
+
+		# read the file into a string
 		questionsFile = fo.read()
+
+		# close the file
 		fo.close()
+
+		# create a dictionary from the string using create_dict
 		questionDict = create_dict(questionsFile)
+
+	# if the file does not exist, create an empty dictionary
 	else:
 		questionDict = {}
+
+	# return the dictionary
 	return questionDict
+
+
+# get the question and choices and return them as a ascii value
+def get_question_input():
+	goodInput = False
+
+	chosenAnswer = input("Enter Choice: ")
+
+	try:
+		chosenAnswer = str(chosenAnswer)
+		fixedAnswer = chosenAnswer.upper()
+
+		asciiAnswer = ord(fixedAnswer)
+
+		if(asciiAnswer >= 65 and asciiAnswer <= 68):
+			goodInput = True
+	except:
+		goodInput = False
+
+	return asciiAnswer, goodInput
+
 
 def file_exists(fileName):
 	try:
@@ -121,15 +174,12 @@ def pick_random(questionDict, pickedQuestions, isChoice):
 		if(isChoice):
 			randomChoice = list(questionDict.values())[randomNumber]
 
-			print("Random Value: " + randomChoice)
 		else:
 			randomChoice = list(questionDict.keys())[randomNumber]
-			print("Random Key: " + randomChoice)
 
 		if(randomChoice not in pickedQuestions):
 			isLooping = False
 
-	print("Picked Questions: " + str(pickedQuestions))
 	return randomChoice
 
 def pick_choices(chosenQuestion, questionsDict):
@@ -157,6 +207,8 @@ def create_question(chosenQuestion, choices):
 	return question
 
 def check_answer(chosenAnswer, choices, questionsDict, chosenQuestion):
+	usrAnswer = ""
+
 	if(chosenAnswer == ord("A")):
 		usrAnswer = choices[0]
 	elif(chosenAnswer == ord("B")):
