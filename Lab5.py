34be7a0f6703e5bd4114eabcad639abf43c57647
MattLD13 <mdicks2025@pingry.org>
2023-05-22 12:52:20 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -11,8 +11,8 @@ def main():
 
 	questionAmt = 0
 	score = 0
-	pickedChoices = {}
-	choices = {}
+	pickedQuestions = []
+	choices = []
 	fileExists = True
 
 	welcome_message()
@@ -25,17 +25,24 @@ def main():
 
 	while(questionAmt < 10):
 		print("Question " + str(questionAmt + 1) + ":")
-			
-		#print(questionsDict)
 
-		chosenQuestion, pickedChoices  = pick_question(questionsDict, pickedChoices)
+		chosenQuestion = pick_random(questionsDict, pickedQuestions)
 
-		choices = pick_choices(chosenQuestion, questionsDict, pickedChoices)
+		pickedQuestions.append(chosenQuestion)
+
+		print("after" + str(pickedQuestions))
+
+		choices = pick_choices(chosenQuestion, questionsDict)
 
 		question = create_question(chosenQuestion, choices)
 		print(question)
 
 		chosenAnswer = get_question_input()
+		if(chosenAnswer == 0):
+			print("Please enter a valid choice (A, B, C, or D).")
+			print()
+			print(question)
+			chosenAnswer = get_question_input()
 
 		isCorrect = check_answer(chosenAnswer, choices, questionsDict, chosenQuestion)
 		
@@ -62,7 +69,6 @@ def get_initial_input():
 	return fileName
 
 def get_question_input():
-
 	isLooping = True
 	while isLooping:
 		usrInput = input("Enter Choice: ")
@@ -76,7 +82,7 @@ def get_question_input():
 			chosenAnswer = ord(usrInput)
 			return chosenAnswer
 		else:
-			print("Please enter a valid choice (A, B, C, or D).")
+			return 0
 
 def read_file(fileName):
 	if file_exists(fileName):
@@ -105,70 +111,50 @@ def create_dict(questions):
 		questionDict[question[0]] = question[1]
 	return questionDict
 
-def pick_random(length):
-	chosenNumber = random.randint(0, length - 1)
-	return chosenNumber
-
-def pick_question(questionDict, pickedChoices):
+def pick_random(questionDict, pickedQuestions):
 	isLooping = True
-	while isLooping:
-		chosenNumber = pick_random(len(questionDict))
-		chosenQuestion = list(questionDict.keys())[chosenNumber]
-		if(chosenQuestion not in pickedChoices):
+
+	print("ahhhhhhh" + str(pickedQuestions))
+
+	while(isLooping):
+		randomChoice = random.randint(0, len(questionDict) - 1)
+		if(randomChoice not in pickedQuestions):
 			isLooping = False
 
-	pickedChoices[chosenQuestion] = questionDict[chosenQuestion]
-	return chosenQuestion, pickedChoices
+	chosenQuestion = list(questionDict.keys())[randomChoice]
 
-def pick_choices(chosenQuestion, questionsDict, pickedChoices):
-	choices = {}
+	return chosenQuestion
+
+def pick_choices(chosenQuestion, questionsDict):
+	choices = []
 	# pick 3 random choices
 	for i in range(3):
-		randomNum = pick_random(len(questionsDict))
-		# if the random number chosen is the same as the chosen question, pick again
-		while(randomNum == chosenQuestion):
-			randomNum = pick_random(len(questionsDict))
-		# if the choice has already been picked, pick again
-		while(questionsDict[list(questionsDict.keys())[randomNum]] in choices.values()):
-			randomNum = pick_random(len(questionsDict))
+		randomChoice = pick_random(questionsDict, choices)
+		if(randomChoice != chosenQuestion):
+			choices.append(questionsDict[randomChoice])
 
-		# add the choice to the choices dictionary
-		choices[list(questionsDict.keys())[randomNum]] = questionsDict[list(questionsDict.keys())[randomNum]]
+	randomChoice = random.randint(0, 3)
+	choices.insert(randomChoice, questionsDict[chosenQuestion])
 
-	# add the chosen question to the choices dictionary
-	choices[chosenQuestion] = questionsDict[chosenQuestion]
-
-	# convert the dictionary to a list so it can be shuffled
-	choices = list(choices.items())
-
-	# shuffle the list
-	for num in range(len(choices) - 1, 0, -1):
-		randNum = random.randint(0, num)
-		choices[num], choices[randNum] = choices[randNum], choices[num]
-
-	# convert the list back to a dictionary
-	choices = dict(choices)
-
-	print(choices)
 	return choices
 
 def create_question(chosenQuestion, choices):
-	
-	question = chosenQuestion + "\n" + "A. " + choices[list(choices.keys())[0]] + "\n" + "B. " + choices[list(choices.keys())[1]] + "\n" + "C. " + choices[list(choices.keys())[2]] + "\n" + "D. " + choices[list(choices.keys())[3]] + "\n"
+
+	question = chosenQuestion + "\n" + "A. " + choices[0] + "\n" + "B. " + choices[1] + "\n" + "C. " + choices[2] + "\n" + "D. " + choices[3]
 
 	return question
 
 def check_answer(chosenAnswer, choices, questionsDict, chosenQuestion):
 	if(chosenAnswer == ord("A")):
-		chosenAnswer = list(choices.keys())[0]
+		usrAnswer = choices[0]
 	elif(chosenAnswer == ord("B")):
-		chosenAnswer = list(choices.keys())[1]
+		usrAnswer = choices[1]
 	elif(chosenAnswer == ord("C")):
-		chosenAnswer = list(choices.keys())[2]
+		usrAnswer = choices[2]
 	elif(chosenAnswer == ord("D")):
-		chosenAnswer = list(choices.keys())[3]
+		usrAnswer = choices[3]
 
-	if(chosenAnswer == chosenQuestion):
+	if(usrAnswer == questionsDict[chosenQuestion]):
 		isCorrect = True
 	else:
 		isCorrect = False
